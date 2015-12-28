8ea3653769465cf2823d91d62f0331951530c080
Jan Michael Raymundo <j.raymundo@klab.com>
2015-12-29 06:00:04 +0800

    Updated manifest to fix issue with orientation of game screen.

--- BBallStatsTrack/src/com/example/bballstatstrack/activities/GameActivity.java
+++ BBallStatsTrack/src/com/example/bballstatstrack/activities/GameActivity.java
@@ -1,22 +1,24 @@
 package com.example.bballstatstrack.activities;
 
+import com.example.bballstatstrack.R;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class GameActivity extends Activity
 {
-    public static final String EXTRA_GAME_ID = "gameID";
+	public static final String EXTRA_GAME_ID = "gameID";
 
-    @Override
-    protected void onCreate( Bundle savedInstanceState )
-    {
-        super.onCreate( savedInstanceState );
-        Intent intent = getIntent();
-        int[] homeNumbers = intent.getIntArrayExtra( AddPlayersToTeamsActivity.HOME_TEAM_NUMBERS );
-        String[] homeNames = intent.getStringArrayExtra( AddPlayersToTeamsActivity.HOME_TEAM_NAMES );
-        int[] awayNumbers = intent.getIntArrayExtra( AddPlayersToTeamsActivity.AWAY_TEAM_NUMBERS );
-        String[] awayNames = intent.getStringArrayExtra( AddPlayersToTeamsActivity.AWAY_TEAM_NAMES );
-        System.out.println();
-    }
+	@Override
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		Intent intent = getIntent();
+		int[] homeNumbers = intent.getIntArrayExtra( AddPlayersToTeamsActivity.HOME_TEAM_NUMBERS );
+		String[] homeNames = intent.getStringArrayExtra( AddPlayersToTeamsActivity.HOME_TEAM_NAMES );
+		int[] awayNumbers = intent.getIntArrayExtra( AddPlayersToTeamsActivity.AWAY_TEAM_NUMBERS );
+		String[] awayNames = intent.getStringArrayExtra( AddPlayersToTeamsActivity.AWAY_TEAM_NAMES );
+		setContentView( R.layout.activity_game );
+	}
 }
