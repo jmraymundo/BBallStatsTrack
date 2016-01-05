a110c3743c8831b7c5efa4ff330ac753467bfa00
Jan Michael Raymundo <j.raymundo@klab.com>
2016-01-05 15:16:37 +0800

    Some minor corrections
    Removed append override since it's not needed.
    Updated toString since it doesn't append anything.

--- BBallStatsTrack/src/com/example/bballstatstrack/models/gameevents/foulevents/NonShootingFoulEvent.java
+++ BBallStatsTrack/src/com/example/bballstatstrack/models/gameevents/foulevents/NonShootingFoulEvent.java
@@ -3,8 +3,6 @@ package com.example.bballstatstrack.models.gameevents.foulevents;
 import com.example.bballstatstrack.models.Player;
 import com.example.bballstatstrack.models.Team;
 import com.example.bballstatstrack.models.gameevents.FoulEvent;
-import com.example.bballstatstrack.models.gameevents.GameEvent;
-import com.example.bballstatstrack.models.gameevents.ShootEvent;
 
 public class NonShootingFoulEvent extends FoulEvent
 {
@@ -25,36 +23,9 @@ public class NonShootingFoulEvent extends FoulEvent
     }
 
     @Override
-    public void append( GameEvent appendedEvent )
-    {
-        if( mAppended != null )
-        {
-            mAppended.append( appendedEvent );
-            return;
-        }
-        if( appendedEvent instanceof ShootEvent )
-        {
-            switch( mNonShootingFoulType )
-            {
-                case NON_PENALTY:
-                    return;
-                case PENALTY:
-                    mAppended = appendedEvent;
-            }
-            return;
-        }
-        super.append( appendedEvent );
-    }
-
-    @Override
     public String toString()
     {
-        String output = mPlayer.getFullName() + " committed a foul. ";
-        if( mAppended != null )
-        {
-            output = output.concat( mAppended.toString() );
-        }
-        return output.trim();
+        return mPlayer.getFullName() + " committed a foul.";
     }
 
     @Override
